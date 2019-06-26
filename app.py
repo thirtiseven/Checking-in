75d4e662ce648095304de2445fc7f0f6a3817651
thirtiseven lee <ntlihy@gmail.com>
2019-06-26 08:39:38 +0800

    add

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
 
-from Checking_in import app, db
+from Checking_in import app, db, login_manager
 
 if __name__ == '__main__':
     app = Flask(__name__)
+    login_manager.login_view = 'login'
     Bootstrap(app)
     app.run()
